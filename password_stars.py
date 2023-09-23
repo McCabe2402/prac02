6727091149525ac0c6c5dfd441b7ec5c66a4e202
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2023-09-23 18:22:58 +1000

    Refactor password check program to use functions

--- password_stars.py
+++ password_stars.py
@@ -1,23 +1,38 @@
 # Set the minimum password length
 minimum_length = 8
 
-# Initialize a variable to keep track of whether the password is valid
-is_valid = False
 
-while not is_valid:
-    # Ask the user for a password
-    password = input("Enter a password: ")
+# Function to get the password from the user
+def get_password():
+    while True:
+        password = input("Enter a password: ")
+        if len(password) >= minimum_length:
+            return password
+        else:
+            print(f"Password must be at least {minimum_length} characters long. Try again.")
 
-    # Check if the password meets the minimum length requirement
-    if len(password) >= minimum_length:
-        is_valid = True
-        print("Password is valid!")
 
-        # Print asterisks for each character in the password
-        i = 0
-        while i < len(password):
-            print("*", end="")
-            i += 1
-        print()  # Print a newline to move to the next line
-    else:
-        print(f"Password must be at least {minimum_length} characters long. Try again.")
+# Function to print asterisks for a given password
+def print_asterisks(password):
+    print("Password is valid!")
+
+    # Print asterisks for each character in the password
+    for _ in password:
+        print("*", end="")
+    print()  # Print a newline to move to the next line
+
+
+# Function to check password validity
+def check_password():
+    password = get_password()
+    print_asterisks(password)
+
+
+# Define the main function
+def main():
+    check_password()
+
+
+# Call the main function
+if __name__ == "__main__":
+    main()
